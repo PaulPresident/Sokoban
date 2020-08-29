81c75846f02bd4afc36b807bf0383b0e49c597d0
PaulPresident <47800285+PaulPresident@users.noreply.github.com>
2020-08-29 18:14:41 -0400

    v0.1.4
    Pre_Release
    
    Versoin 0.1.4 will be all about design
    
    Changes
    -- Main Menu Buttons Added

--- main.py
+++ main.py
@@ -18,25 +18,33 @@ display.set_caption('Sokoban')
 width, height = 1600, 900
 
 main_menu = display.set_mode((width, height))
+options = display.set_mode((width, height))
 game_screen = display.set_mode((width, height))
 
 in_main_menu = True
+in_options = False
 in_game = False
 
-movement = {
-    pygame.K_w:pygame.K_s, pygame.K_s:pygame.K_w,
-    pygame.K_a: pygame.K_d, pygame.K_d: pygame.K_a
-} # TODO find better place to store under movement class maybe
+game_title = Button("resources/images/Game Title.png", (width*(203/960), height*(71/540)))
+play_button = Button("resources/images/Play Button.png", (width*(320/960), height*(220/540)))
+options_button = Button("resources/images/Options Button.png", (width*(361/960), height*(345/540)))
+exit_button = Button("resources/images/Exit Button.png", (width*(402/960), height*(443/540)))
 
-play_button = Button("resources/images/Play Button.png", (200, 140))
+def resize():
+    game_title.resize(width, height)
+    play_button.resize(width, height)
+    options_button.resize(width, height)
+    exit_button.resize(width, height)
 
+resize()
 
 while in_main_menu:
     main_menu.fill((255, 255, 255))
 
+    game_title.draw(screen=main_menu)
     play_button.draw(screen=main_menu)
-    pygame.draw.rect(main_menu, (255, 0, 0), pygame.Rect(240, 160, 800, 200), 1) # ! for refrence only
-    pygame.draw.rect(main_menu, (255, 0, 0), pygame.Rect(480, 480, 320, 120), 1) # ! for refrence only
+    options_button.draw(screen=main_menu)
+    exit_button.draw(screen=main_menu)
 
     pygame.display.flip()
 
@@ -44,6 +52,28 @@ while in_main_menu:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if play_button.is_clicked(mouse_pos=pygame.mouse.get_pos()):
+                in_main_menu = False
+                in_game = True
+            if options_button.is_clicked(mouse_pos=pygame.mouse.get_pos()):
+                in_main_menu = False
+                in_options = True
+            if exit_button.is_clicked(mouse_pos=pygame.mouse.get_pos()):
+                pygame.quit()
+                exit(0)
+
+while in_options:
+    options.fill((255, 255, 255))
+
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+
 # # TODO figure out resizing to specific sizes (1920, 1280|1600, 900|1280, 720)
         # if event.type == pygame.VIDEORESIZE:
 #             main_menu = pygame.display.set_mode(event.size, HWSURFACE|DOUBLEBUF|RESIZABLE)
@@ -55,15 +85,6 @@ while in_main_menu:
 #             height = event.h
 #             # pygame.transform.scale(main_menu, event.dict['size'], event.dict['size'])
 #             # pygame.display.flip()
-
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x, y = pygame.mouse.get_pos()
-            mouse_rect = pygame.Rect(x, y, 0, 0)
-            if mouse_rect.colliderect(play_button.hitbox):
-                in_main_menu = False
-                in_game = True
-
-
 
 # ! this will be an image
 tile_w, tile_h = 60, 60
--- sokoban/button.py
+++ sokoban/button.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Button():
-    def __init__(self, image:str, location:tuple):
+    def __init__(self, image:str, location:tuple, size:int=None):
         self.image = pygame.image.load(image)
         self.width, self.height = self.image.get_width(), self.image.get_height()
         self.x, self.y = location
@@ -10,10 +10,19 @@ class Button():
     def hitbox(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
 
+    def resize(self, width, height):
+        size = (round(self.width/(1920/width)), round(self.height/(1080/height)))
+        self.image = pygame.transform.scale(self.image, size)
+        self.width, self.height = self.image.get_width(), self.image.get_height()
+
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
         screen.blit(self.image, (self.x, self.y))
 
-    def resize(self, screen, w, h):
-        self.image = pygame.transform.scale(self.image, (self.width + w, self.height + h))
-        self.draw(screen=screen)
+    def is_clicked(self, mouse_pos):
+        mouse_rect = pygame.Rect(*mouse_pos, 0, 0)
+        return mouse_rect.colliderect(self.hitbox)
+
+    @classmethod
+    def medium(cls, image:str, location:tuple):
+        cls(image=image, location=location)
